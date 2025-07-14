7a6476b48f5db6f10394ab087d5f38c5603784e4
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-07-15 00:13:01 +0700

    feat: update.

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ async function main() {
     // 2. Register Service Worker
     if ('serviceWorker' in navigator) {
         try {
-            await navigator.serviceWorker.register('./service-worker.js');
+            await navigator.serviceWorker.register('../service-worker.js');
             console.log('Service Worker registered successfully.');
         } catch (error) {
             console.error('Service Worker registration failed:', error);
