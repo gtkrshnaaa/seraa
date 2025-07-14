facb7fef650a1cfd4a3bca64f3bc2831c18a4967
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-07-15 00:35:20 +0700

    feat: update.

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-latest:generateContent?key=";
+const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=";
 
 export async function callGemini(prompt, apiKey) {
     try {
