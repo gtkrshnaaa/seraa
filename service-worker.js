825dc28a44a1424b41bbba5dc14408f639dfaaea
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-08-20 07:15:16 +0700

    feat: update.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // File: service-worker.js
 
-const CACHE_NAME = 'seraa-cache-v49-vue'; // Version bump for new architecture
+const CACHE_NAME = 'seraa-cache-v50-vue'; // Version bump for new architecture
 const URLS_TO_CACHE = [
     'index.html',
     'manifest.json',
